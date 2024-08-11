46f8a7a3d1a469e596cef0192bcacd1d1a1ba0dc
skookumchoocher <natebrooks004@gmail.com>
2024-08-11 10:54:03 -0500

    docs: add godoc comment for add func

--- ch10/math.go
+++ ch10/math.go
@@ -1,5 +1,7 @@
 package ch10
 
+// Add takes two integers and returns the sum of them.
+// Go to https://www.mathsisfun.com/numbers/addition.html for more information.
 func Add(a, b int) int {
 	return a + b
 }
