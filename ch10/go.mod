5b93a3b9c41411d199bd00e61d7aef575670ac7c
skookumchoocher <natebrooks004@gmail.com>
2024-08-11 10:59:07 -0500

    BREAKING CHANGE: make go generic

--- ch10/go.mod
+++ ch10/go.mod
@@ -1,3 +1,5 @@
 module github.com/N8Brooks/learninggo/ch10
 
 go 1.21.3
+
+require golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa
--- ch10/math.go
+++ ch10/math.go
@@ -1,7 +1,13 @@
 package ch10
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add takes two integers and returns the sum of them.
 // Go to https://www.mathsisfun.com/numbers/addition.html for more information.
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
